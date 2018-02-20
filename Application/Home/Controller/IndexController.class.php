6bcbceb2889e725aa3dfa665254e452df22901f5
1070794219 <1070794219@qq.com>
2018-02-20 19:42:33 +0800

    修改首页页面新闻排列

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends CommonController {
     public function index(){
-    	$this->assign('arclist',M('Article')->where('type = 0')->limit(9)->select());
+    	$this->assign('arclist',M('Article')->where('type = 0')->order('time desc')->limit(9)->select());
     	$this->display();
     }
 }
